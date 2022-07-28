9ca887f996dcf830c5de664da52743ef5cb2a591
Tarasov Vladyslav <vlad@glaps.fun>
2022-07-28 14:39:48 +0300

    fix

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.10-alpine3.16 as builder
+WORKDIR /app
+RUN apk update \
+    && apk add --no-cache make automake gcc g++ subversion python3-dev \
+    && rm -vrf /var/cache/apk/*
+COPY requirements.txt .
+RUN pip wheel --no-cache-dir --no-deps --wheel-dir /app/wheels -r requirements.txt
+
+FROM python:3.10-alpine3.16
+WORKDIR /app
+COPY --from=builder /app/wheels /wheels
+RUN pip install --no-cache /wheels/*
+COPY . .
+EXPOSE 8000/tcp
+ENTRYPOINT [ "uvicorn","--host", "0.0.0.0","--port", "8000"]
+CMD ["main:app"]
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from fastapi import FastAPI, Header, Request
-from fastapi.responses import HTMLResponse
+import platform
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 async def root(r: Request):
-    return templates.TemplateResponse("index.html", {"request": r, "headers": r.headers.items()})
-
-
-@app.get("/heders")
-async def get_heder(r: Request):
-    for i in r.headers.items():
-        print(i[0], "=", i[1])
-    return {'headers': list(r.headers.items())}
+    hostname = platform.node()
+    data = r.headers.items() + \
+        [("remoteAddr", r.client.host), ("remotePort", r.client.port)]
+    return templates.TemplateResponse("index.html", {"request": r, 'hostname': hostname, "headers": data})
