a6e5b9f19ee4aaaa2034a7e8e56e80039f12b977
Tarasov Vladyslav <vlad@glaps.fun>
2022-07-28 21:42:43 +0300

    add ico and add ip rout

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import platform
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+class ip(BaseModel):
+    ip: str
 
 
 @app.get("/")
@@ -14,3 +20,8 @@ async def root(r: Request):
     data = r.headers.items() + \
         [("remoteAddr", r.client.host), ("remotePort", r.client.port)]
     return templates.TemplateResponse("index.html", {"request": r, 'hostname': hostname, "headers": data})
+
+
+@app.get("/ip")
+async def get_ip(r: Request):
+    return {"IP": r.headers["x-real-ip"] if "x-real-ip" in r.headers.keys() else r.client.host}
