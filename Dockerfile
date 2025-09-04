3514a3fdf53005b35d76550198089af911c17a7c
vt46 <vt@playa.tech>
2025-09-04 17:18:14 +0300

    chore: update code structure and remove redundant changes

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-FROM python:3.11.9-alpine3.20 as builder
+
+FROM python:3.13-alpine3.21 AS builder
 WORKDIR /app
 RUN apk update \
     && apk add --no-cache musl-dev linux-headers make automake gcc g++ subversion python3-dev \
     && rm -vrf /var/cache/apk/*
 COPY . .
-RUN pip wheel --no-cache-dir --wheel-dir /app/wheels -r requirements.txt
 RUN pip wheel --no-cache-dir --wheel-dir /app/wheels .
 
-FROM python:3.11.9-alpine3.20 
+FROM python:3.13-alpine3.21
 WORKDIR /app
-COPY --from=builder /app/wheels /wheels
-RUN pip install --no-cache /wheels/* && rm -rf /wheels
+COPY --from=builder /app/wheels/ /tmp/wheels
+RUN ls -la
+RUN pip install --no-cache /tmp/wheels/* && rm -rf /tmp/wheels
 EXPOSE 8000/tcp
 ENTRYPOINT ["responcer"]
--- responcer/run.py
+++ responcer/run.py
@@ -1,3 +1,6 @@
+
+
+import uvicorn
 import platform
 import psutil
 import pathlib
@@ -5,18 +8,17 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-import uvicorn
 
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 app.mount(
-    "/static",
-    StaticFiles(directory=pathlib.Path(__file__).parent / "static"),
+    path="/static",
+    app=StaticFiles(directory=pathlib.Path(__file__).parent / "static"),
     name="static",
 )
 
 
-templates = Jinja2Templates(directory=pathlib.Path(__file__).parent / "templates")
+templates: Jinja2Templates = Jinja2Templates(directory=pathlib.Path(__file__).parent / "templates")
 
 
 class ip(BaseModel):
@@ -24,53 +26,56 @@ class ip(BaseModel):
     port: int
 
 
-@app.get("/")
-async def root(r: Request):
-    hostname = platform.node()
-    data = r.headers.items() + [
-        ("remoteAddr", r.client.host),
-        ("remotePort", r.client.port),
-    ]
-    return templates.TemplateResponse(
-        "index.html", {"request": r, "hostname": hostname, "headers": data}
-    )
-
-
-@app.get("/api/v1/ip")
-async def get_ip(r: Request):
+def help_get_ip(r: Request):
     return {
         "IP": (
-            r.headers["x-real-ip"] if "x-real-ip" in r.headers.keys() else r.client.host
+            r.headers["x-real-ip"] if "x-real-ip" in r.headers.keys() else r.client.host # pyright: ignore[reportOptionalMemberAccess]
         )
     }
 
+@app.get(path="/")
+async def root(r: Request) :
+    hostname: str = platform.node()
+    real_ip: dict[str, str] = help_get_ip(r)
+    data= r.headers.items() + [
+        ("remoteAddr", r.client.host), # pyright: ignore[reportOptionalMemberAccess]
+        ("remotePort", r.client.port), # pyright: ignore[reportOptionalMemberAccess]
+    ]
+    return templates.TemplateResponse(
+        "index.html", {"request": r, "ip": real_ip["IP"] ,"hostname": hostname, "headers": data}
+    )
 
-@app.get("/api/v1/version")
-async def version(r: Request):
+
+@app.get(path="/api/v1/ip")
+async def get_ip(r: Request) -> dict[str, str]:
+    return help_get_ip(r)
+
+@app.get(path="/api/v1/version")
+async def version(r: Request) -> dict[str, str]:
+    print(r)
     return {"Version": "version 1.0.0"}
 
 
-@app.get("/metrics")
-async def metrics(r: Request):
+@app.get(path="/metrics")
+async def metrics(r: Request) -> dict[str, str | float | int | None]:
     return {
         "node_name": platform.node(),
         "processor": platform.processor(),
         "system": platform.system(),
         "cpu_percent": psutil.cpu_percent(),
         "cpu_count": psutil.cpu_count(),
-        "memory": psutil.virtual_memory().percent,
-        "x-real-ip": (
-            r.headers["x-real-ip"] if "x-real-ip" in r.headers.keys() else r.client.host
-        ),
+        "memory": psutil.virtual_memory().percent,  # pyright: ignore[reportAny]
+        "x-real-ip": help_get_ip(r)["IP"]
     }
 
 
-@app.get("/health")
-async def health(r: Request):
+@app.get(path="/health")
+async def health(r: Request) -> dict[str, bool]:
+    print(r)
     return {"health": True}
 
 
-def main():
+def main() -> None:
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
